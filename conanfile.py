9da974df4479ec0efeb9549e6bec49584a8a7ef1
Jerry W <jerrywiltse@gmail.com>
2017-10-01 17:08:32 -0400

    add MSYS_ROOT environment variable

--- conanfile.py
+++ conanfile.py
@@ -35,4 +35,9 @@ class MSYS2Conan(ConanFile):
             raise Exception("unsupported architecture %s" % self.settings.arch)
 
     def package_info(self):
-        self.env_info.path.append(os.path.join(self.package_folder, "usr", "bin"))
+        bin_path = os.path.join(self.package_folder, "usr", "bin")
+        self.output.info("Appending PATH environment variable with : {0}".format(bin_path))
+        self.env_info.path.append(bin_path)
+        
+        self.output.info("Creating MSYS_ROOT environment variable with : {0}".format(self.package_folder))
+        self.env_info.MSYS_ROOT = self.package_folder
