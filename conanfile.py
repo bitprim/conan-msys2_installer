cbd6099981e0beeb3ef5699444da49d10eff4fb1
Jerry W <jerrywiltse@gmail.com>
2017-10-01 16:04:21 -0400

    added checksum and no_copy_source

--- conanfile.py
+++ conanfile.py
@@ -8,14 +8,16 @@ class MSYS2Conan(ConanFile):
     license = "MSYS license"
     description = "MSYS2 is a software distro and building platform for Windows"
     url = "https://github.com/bincrafters/conan-msys2_installer"
+    no_copy_source = True
     settings = {"os": ["Windows"], "arch": ["x86", "x86_64"]}
-    
     
     def source(self):
         if self.settings.arch == "x86_64":
-            msys2_arch = 'x86_64'
+            msys2_arch = "x86_64"
+            checksum = "2c198787ea1c4be39ff80466c4d831f8c7f06bd56d6d190bf63ede35292e344c"
         elif self.settings.arch == "x86":
-            msys2_arch = 'i686'
+            msys2_arch = "i686"
+            checksum = "4951a47177777a54c7ad4ac99755ba4bbdf1a0cb23a174a72d91f71dc25bcb15"
         else:
             raise Exception("unsupported architecture %s" % self.settings.arch)
 
@@ -23,6 +25,7 @@ class MSYS2Conan(ConanFile):
         url = "http://repo.msys2.org/distrib/%s/%s" % (msys2_arch, archive_name)
         self.output.warn("download %s into %s" % (url, archive_name))
         tools.download(url, archive_name, verify=True)
+        tools.check_sha256(archive_name, checksum)
         tools.untargz(archive_name)
         os.unlink(archive_name)
         
