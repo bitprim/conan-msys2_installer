eeb4d6d1b8956f1c096799b9194e859b6940b2e2
SSE4 <tomskside@gmail.com>
2017-11-05 21:05:20 +0700

    - escape quotes

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,9 +7,9 @@ class TestPackage(ConanFile):
     def test(self):
         new_path = os.environ['PATH'] + os.pathsep + self.deps_env_info['msys2_installer'].MSYS_BIN
         with tools.environment_append({'PATH': new_path}):
-            self.run('%MSYS_BIN%\\bash -c "yasm --version"')
-            self.run('%MSYS_BIN%\\bash -c "make --version"')
-            self.run('%MSYS_BIN%\\bash -c "diff --help"')
-            self.run('%MSYS_BIN%\\bash -c "pkg-config --version"')
-            self.run('%MSYS_BIN%\\bash -c "autoconf --version"')
-            self.run('%MSYS_BIN%\\bash -c "autoreconf --version"')
+            self.run('%MSYS_BIN%\\bash -c ^"yasm --version^"')
+            self.run('%MSYS_BIN%\\bash -c ^"make --version^"')
+            self.run('%MSYS_BIN%\\bash -c ^"diff --help^"')
+            self.run('%MSYS_BIN%\\bash -c ^"pkg-config --version^"')
+            self.run('%MSYS_BIN%\\bash -c ^"autoconf --version^"')
+            self.run('%MSYS_BIN%\\bash -c ^"autoreconf --version^"')
