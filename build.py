88544159c57f6d7822e79695d3c617542f7a1662
SSE4 <tomskside@gmail.com>
2017-11-11 13:44:24 +0700

    - upload only stable

--- build.py
+++ build.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from conan.packager import ConanMultiPackager, os, re
 import platform
     
@@ -19,9 +22,11 @@ if __name__ == "__main__":
         os.environ["CONAN_USERNAME"] = username
         os.environ["CONAN_CHANNEL"] = channel
         os.environ["CONAN_REFERENCE"] = "{0}/{1}".format(name, version)
-        os.environ["CONAN_UPLOAD"]="https://api.bintray.com/conan/{0}/public-conan".format(username)
-        os.environ["CONAN_REMOTES"]="https://api.bintray.com/conan/conan-community/conan"
+        os.environ["CONAN_UPLOAD"] = "https://api.bintray.com/conan/{0}/public-conan".format(username)
+        os.environ["CONAN_REMOTES"] = "https://api.bintray.com/conan/conan-community/conan"
+        os.environ["CONAN_UPLOAD_ONLY_WHEN_STABLE"] = "1"
+        os.environ["CONAN_STABLE_BRANCH_PATTERN"] = "stable/*"
     
     builder = ConanMultiPackager()
-    builder.add({"arch" : os.environ["CONAN_ARCHS"]}, {}, {}, {}) 
+    builder.add({"arch": os.environ["CONAN_ARCHS"]}, {}, {}, {})
     builder.run()
